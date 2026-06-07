c4b94ddcb90547a1009a4ce3377a4fb47c602401
mahdiridoy <59791566+mahdiridoy@users.noreply.github.com>
2026-06-08 02:39:16 +0600

    Add new keywords for various categories

--- config.py
+++ config.py
@@ -1,32 +1,56 @@
 BD_KEYWORDS = [
     "BTV","ATN","Channel i","Jamuna","Somoy","Ekattor",
-    "NTV","Bangla","Boishakhi","Desh","Gazi","DBC"
+    "NTV","Bangla","Boishakhi","Desh","Gazi","DBC","Mytv","SA TV","RTV"
 ]
 
 INDIA_KEYWORDS = [
     "Star Plus","Star Jalsha","Zee","Sony","Colors",
-    "Sun TV","Sony SAB","DD National","Sony Ten"
+    "Sun TV","Sony SAB","DD National","Sony Ten","Aaj Tak",
+    "Republic","India TV","Zee News","NDTV","TV9","ABP"
 ]
 
 CARTOON_KEYWORDS = [
     "Cartoon Network","Nick","Nickelodeon","Disney",
-    "Pogo","Hungama","BabyTV","Toon","CBeebies"
+    "Pogo","Hungama","BabyTV","Toon","CBeebies","Baby","Kids"
+]
+
+NEWS_KEYWORDS = [
+    "News","Samachar","Khobor","BBC","CNN","Al Jazeera",
+    "CNBC","Bloomberg","Sky News","France 24","DW","Euronews"
+]
+
+SPORTS_KEYWORDS = [
+    "Sport","ESPN","Star Sports","Sony Six","Ten Sports",
+    "Willow","DSport","Cricket","Football","FIFA","Sky Sports",
+    "BT Sport","Sony LIV Sports"
+]
+
+MOVIES_KEYWORDS = [
+    "Movies","Cinema","Film","Zee Cinema","Star Gold",
+    "Sony Max","B4U","Action","Thriller","Romedy","&Pictures"
+]
+
+MUSIC_KEYWORDS = [
+    "Music","MTV","VH1","Channel V","Zing","9X Jalwa",
+    "Hits","Beats","Radio"
 ]
 
 ADULT_KEYWORDS = [
-    "adult",
-    "xxx",
-    "porn",
-    "sex",
-    "private",
-    "dorcel",
-    "penthouse",
-    "playboy",
-    "hustler",
-    "boyxx",
-    "sextreme",
-    "brazzers",
-    "redlight",
-    "erotic",
-    "18+"
+    "adult","xxx","porn","sex","private","dorcel",
+    "penthouse","playboy","hustler","boyxx","sextreme",
+    "brazzers","redlight","erotic","18+"
 ]
+
+# Output category display names & order
+CATEGORY_ORDER = ["bd","india","cartoon","news","sports","movies","music","other"]
+
+CATEGORY_LABELS = {
+    "bd"      : "Bangladesh",
+    "india"   : "India",
+    "cartoon" : "Kids & Cartoon",
+    "news"    : "News",
+    "sports"  : "Sports",
+    "movies"  : "Movies",
+    "music"   : "Music",
+    "other"   : "Other",
+}
